dd6354869c077c8049a6038e9283b6a37bd213a9
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-11-30 11:55:32 +0200

    exercise 10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,24 +1,29 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet,ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme.js';
 import AppBarTab from './AppBarTab.jsx';
 import { Link } from "react-router-native";
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appbarBackgroundColor,
   },
+  scrollView: {
+    flexDirection: 'row',
+  },
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
-    <Link to="/">
-      <AppBarTab text="Repositories" />
-    </Link>
-    <Link to="/signin">
-      <AppBarTab text="Sign in" />
-    </Link>
-    
+    <ScrollView horizontal style={styles.scrollView}>{}
+      <Link to="/">
+        <AppBarTab text="Repositories" />
+      </Link>
+      <Link to="/signin">
+        <AppBarTab text="Sign in" />
+      </Link> 
+    </ScrollView>
   </View>;
 };
 
