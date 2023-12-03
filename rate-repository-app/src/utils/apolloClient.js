61154ffcbb845f640688c759adebfd713746f050
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-12-03 20:46:03 +0200

    exercise 10.12

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,15 +1,17 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-  // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.1.101:4000/graphql',
+  uri:Constants.manifest.extra.apolloUri, 
 });
 
 const createApolloClient = () => {
+
   return new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache(),
   });
+  
 };
 
 export default createApolloClient;
